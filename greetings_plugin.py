661820e1335a8dd8eee63b90fb52587f6c35ea74
joemarriage <45903919+joemarriage@users.noreply.github.com>
2021-02-03 16:49:40 +0000

    Update greetings_plugin.py

--- greetings_plugin.py
+++ greetings_plugin.py
@@ -7,16 +7,18 @@ import wake_functions as wf
 
 greetings = plugins.Plugin()
 
-
 #Hello World Command
-helloWF = lambda x: wf.key_word(x, ["hello"])
+
+helloWF = [lambda x: wf.multi_key_word(x, ["hello","hi","hiya","gooday"]),
+lambda x: wf.multi_key_word(x, ["good",["morning","day","afternoon","evening"]])]
+
 helloFunc = lambda: plugins.out("Hello")
-helloCommand = plugins.Command([helloWF],helloFunc)
+helloCommand = plugins.Command(helloWF,helloFunc)
 
 greetings.add_command(helloCommand)
 
 
 #Test
-IN = "good morning"
-if helloCommand.check_wake(IN):
-    helloCommand.call_function()
+IN = input()
+if greetings.find_highest_probablity(IN):
+    greetings.mosts_likely_command(IN).call_function()
