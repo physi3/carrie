2b5b5f87b766e138cf07d34048df16b05601a61f
joemarriage <45903919+joemarriage@users.noreply.github.com>
2021-02-03 16:47:53 +0000

    Unable ordering on Multi Key Word wake functions and fix issues in Key Word wake functions.

--- wake_functions.py
+++ wake_functions.py
@@ -4,7 +4,8 @@ Bases for a few frequently used CarrieAIAssistant plugin module command wake fun
 
 def key_word(command_string, key_words, ordered=False):
     """
-    A wake function that searches for key words, a specific order and be specified.
+    A wake function that searches for key words
+    A specific order and be specified (but there may be no repeated words if so)
     Returns chance of waking
     """
     key_words_found = 0
@@ -13,13 +14,14 @@ def key_word(command_string, key_words, ordered=False):
         search_words = [key_words,key_words[ordered_index:]][ordered]
         if i.lower() in map(lambda x: x.lower(),search_words):
             key_words_found += 1
-            ordered_index = i
+            ordered_index = command_string.split(" ").index(i)
     return key_words_found/len(key_words)
 
-def multi_key_word(command_string, key_words):
+def multi_key_word(command_string, key_words, ordered = False):
     """
-    A wake function that searches for key words.
-    Multiple keywords can be in place of one position.
+    A wake function that searches for key words
+    Multiple keywords can be in place of one position
+    A specific order and be specified (but there may be no repeated words if so)
     Returns chance of waking
     """
     key_words_copy = key_words.copy()
@@ -28,5 +30,7 @@ def multi_key_word(command_string, key_words):
             if_list_contin = isinstance(j, list) and i.lower() in map(lambda x: x.lower(),j)
             if_string_contin = isinstance(j, str) and j.lower() == i.lower()
             if if_list_contin or if_string_contin:
+                if ordered:
+                    key_words_copy = key_words_copy[key_words_copy.index(j):]
                 key_words_copy.remove(j)
     return (len(key_words)-len(key_words_copy))/len(key_words)
