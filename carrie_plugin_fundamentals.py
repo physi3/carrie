96a29a352781aec122da55c2c1f43a961df921ac
joemarriage <45903919+joemarriage@users.noreply.github.com>
2021-02-03 16:48:55 +0000

    Add new methods to the plugin class.

--- carrie_plugin_fundamentals.py
+++ carrie_plugin_fundamentals.py
@@ -16,6 +16,20 @@ class Plugin:
         """
         self.commands.append(command)
 
+    def find_highest_probablity(self, command_string):
+        """
+        Check all commands for highest probabilty
+        """
+        return max(map(lambda x: x.check_wake(command_string), self.commands))
+
+    def mosts_likely_command(self, command_string):
+        """
+        Returns the command with the highest probablility of waking
+        """
+        working_funcs = self.commands.copy()
+        working_funcs.sort(key = lambda x: x.check_wake(command_string))
+        return working_funcs[-1]
+
 class Command:
     """
     A command for a CarrieAIAssistant plugin module
