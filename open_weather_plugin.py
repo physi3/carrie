7a25dde652ed995b89f76de83c5e6011492a73c6
joemarriage <45903919+joemarriage@users.noreply.github.com>
2021-02-09 15:12:06 +0000

    Begin 2 new plugins

---
+++ open_weather_plugin.py
@@ -0,0 +1,17 @@
+"""
+Weather plugin for Carrie
+"""
+
+import requests
+
+import carrie_plugin_fundamentals as plugins
+import wake_functions as wf
+
+with open(".weatherapi_key") as api_key_raw:
+    api_key = api_key_raw.read()
+    EMPTY_URL = "http://pro.openweathermap.org/data/2.5/forecast/hourly?lat={}&lon={}&appid={}"
+    URL = EMPTY_URL.format(51.007679,0.434600,api_key)
+    print(URL)
+    f = requests.get(URL)
+
+print(f)
---
+++ time_plugin.py
@@ -0,0 +1,8 @@
+"""
+Time plugin for Carrie
+"""
+
+import time
+
+import carrie_plugin_fundamentals as plugins
+import wake_functions as wf
