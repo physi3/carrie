e4fe74975ada7d7e25704009ed8cc57e5767ac6c
Joe Marriage <45903919+physi3@users.noreply.github.com>
2021-02-09 15:30:27 +0000

    Update open_weather_plugin.py

--- open_weather_plugin.py
+++ open_weather_plugin.py
@@ -10,7 +10,7 @@ import wake_functions as wf
 with open(".weatherapi_key") as api_key_raw:
     api_key = api_key_raw.read()
     EMPTY_URL = "http://pro.openweathermap.org/data/2.5/forecast/hourly?lat={}&lon={}&appid={}"
-    URL = EMPTY_URL.format(51.007679,0.434600,api_key)
+    URL = EMPTY_URL.format(0,0,api_key)
     print(URL)
     f = requests.get(URL)
 
